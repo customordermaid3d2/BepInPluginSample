adef48b3f229633c54c0af77c69484223daef265
lilly <lilly1987@naver.com>
2022-03-04 08:02:48 +0900

    간략화

--- BepInPluginSample/Sample.cs
+++ BepInPluginSample/Sample.cs
@@ -1,8 +1,9 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using COM3D2.LillyUtill;
+using BepInEx.Logging;
 using COM3D2API;
 using HarmonyLib;
+using LillyUtill.MyWindowRect;
 using Newtonsoft.Json;
 using System;
 using System.Collections;
@@ -18,7 +19,7 @@ namespace BepInPluginSample
     class MyAttribute
     {
         public const string PLAGIN_NAME = "Sample";
-        public const string PLAGIN_VERSION = "21.8.15";
+        public const string PLAGIN_VERSION = "22.3.4";
         public const string PLAGIN_FULL_NAME = "COM3D2.Sample.Plugin";
     }
 
@@ -34,107 +35,206 @@ namespace BepInPluginSample
 
         public static Sample sample;
 
-        public static MyLog myLog = new MyLog(MyAttribute.PLAGIN_NAME);
+        public static ManualLogSource log;
+
+
+        private static ConfigFile config;
+
+        private static ConfigEntry<BepInEx.Configuration.KeyboardShortcut> ShowCounter;
+
+
+
+        private static Vector2 scrollPosition;
+
+        // 위치 저장용 테스트 json
+        public static WindowRectUtill myWindowRect;
+
+        public string windowName = MyAttribute.PLAGIN_NAME;
+        public string FullName = MyAttribute.PLAGIN_NAME;
+        public string ShortName = "SP";
+
 
         public Sample()
         {
+            log = Logger;
             sample = this;
         }
 
         /// <summary>
         ///  게임 실행시 한번만 실행됨
         /// </summary>
-        public void Awake()
+        private void Awake()
         {
-            Sample.myLog.LogMessage("Awake",MyUtill.Get_BuildDateTime(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version));
+            Sample.log.LogMessage("Awake");
 
             // 단축키 기본값 설정
             //ShowCounter = Config.Bind("KeyboardShortcut", "KeyboardShortcut0", new BepInEx.Configuration.KeyboardShortcut(KeyCode.Alpha9, KeyCode.LeftControl));
 
-            SampleConfig.Install(Sample.myLog.log);
+            myWindowRect = new WindowRectUtill(config, MyAttribute.PLAGIN_FULL_NAME, MyAttribute.PLAGIN_NAME, "SP");
+            ShowCounter = config.Bind("GUI", "isGUIOnKey", new BepInEx.Configuration.KeyboardShortcut(KeyCode.Alpha9, KeyCode.LeftControl));
 
-            // 기어 메뉴 추가. 이 플러그인 기능 자체를 멈추려면 enabled 를 꺽어야함. 그러면 OnEnable(), OnDisable() 이 작동함
         }
 
-
-
-        public void OnEnable()
+        private void OnEnable()
         {
-            Sample.myLog.LogMessage("OnEnable");
+            Sample.log.LogMessage("OnEnable");
 
-            //SceneManager.sceneLoaded += this.OnSceneLoaded;
+            SceneManager.sceneLoaded += this.OnSceneLoaded;
 
             // 하모니 패치
             harmony = Harmony.CreateAndPatchAll(typeof(SamplePatch));
-
         }
 
         /// <summary>
         /// 게임 실행시 한번만 실행됨
         /// </summary>
-        public void Start()
+        private void Start()
         {
-            Sample.myLog.LogMessage("Start");
+            Sample.log.LogMessage("Start");
 
-            //SampleGUI.Install(gameObject, Config);
-
-            SampleGUI2.Install<SampleGUI2>(gameObject, Config, MyAttribute.PLAGIN_FULL_NAME, MyAttribute.PLAGIN_NAME, "SP", BepInPluginSample.Properties.Resources.icon, new BepInEx.Configuration.KeyboardShortcut(KeyCode.Alpha9, KeyCode.LeftControl));
-
+            // 기어 메뉴 추가. 이 플러그인 기능 자체를 멈추려면 enabled 를 꺽어야함. 그러면 OnEnable(), OnDisable() 이 작동함
+            SystemShortcutAPI.AddButton(MyAttribute.PLAGIN_FULL_NAME, new Action(delegate () { myWindowRect.IsGUIOn = !myWindowRect.IsGUIOn; }), MyAttribute.PLAGIN_NAME + " : " + ShowCounter.Value.ToString(), BepInPluginSample.Properties.Resources.icon);
             //SystemShortcutAPI.AddButton(MyAttribute.PLAGIN_FULL_NAME, new Action(delegate () { enabled = !enabled; }), MyAttribute.PLAGIN_NAME, MyUtill.ExtractResource(BepInPluginSample.Properties.Resources.icon));
         }
-        /*
-        public string scene_name = string.Empty;
 
-        public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
-            Sample.myLog.LogMessage("OnSceneLoaded", scene.name, scene.buildIndex);
             //  scene.buildIndex 는 쓰지 말자 제발
-            scene_name = scene.name;
+            Sample.log.LogMessage($"OnSceneLoaded, {scene.name}, {scene.buildIndex}");
+            // SceneManager.GetActiveScene().name
         }
-        */
+
         /*
         public void FixedUpdate()
         {
 
         }
         */
-        /*
-        public void Update()
+
+        private void Update()
         {
             //if (ShowCounter.Value.IsDown())
             //{
-            //    Sample.myLog.LogMessage("IsDown", ShowCounter.Value.Modifiers, ShowCounter.Value.MainKey);
+            //    MyLog.LogMessage("IsDown", ShowCounter.Value.Modifiers, ShowCounter.Value.MainKey);
             //}
             //if (ShowCounter.Value.IsPressed())
             //{
-            //    Sample.myLog.LogMessage("IsPressed", ShowCounter.Value.Modifiers, ShowCounter.Value.MainKey);
+            //    MyLog.LogMessage("IsPressed", ShowCounter.Value.Modifiers, ShowCounter.Value.MainKey);
             //}
-            //if (ShowCounter.Value.IsUp())
-            //{
-            //    Sample.myLog.LogMessage("IsUp", ShowCounter.Value.Modifiers, ShowCounter.Value.MainKey);
-            //}
+            if (ShowCounter.Value.IsUp())
+            {
+                myWindowRect.IsGUIOn = !myWindowRect.IsGUIOn;
+                Sample.log.LogMessage($"IsUp {ShowCounter.Value.Modifiers} {ShowCounter.Value.MainKey}");
+            }
         }
-        */
+
         /*
         public void LateUpdate()
         {
 
         }
         */
-        
-        /*
-        public void OnGUI()
+
+        private void OnGUI()
         {
-          
+            if (!myWindowRect.IsGUIOn)
+                return;
+
+            myWindowRect.WindowRect = GUILayout.Window(myWindowRect.winNum, myWindowRect.WindowRect, WindowFunction, "", GUI.skin.box);
+        }
+
+        private void WindowFunction(int id)
+        {
+            GUI.enabled = true;
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(myWindowRect.windowName, GUILayout.Height(20));
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("-", GUILayout.Width(20), GUILayout.Height(20))) { myWindowRect.IsOpen = !myWindowRect.IsOpen; }
+            if (GUILayout.Button("x", GUILayout.Width(20), GUILayout.Height(20))) { myWindowRect.IsGUIOn = false; }
+            GUILayout.EndHorizontal();
+
+            if (!myWindowRect.IsOpen)
+            {
+
+            }
+            else
+            {
+                scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+                if (GUILayout.Button("sample1 Start Coroutine"))
+                {
+                    Debug.Log("Button1");
+                    Sample.sample.StartCoroutine(MyCoroutine());
+                }
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("sample5"))
+                {
+                    Debug.Log("Button5");
+                }
+                GUILayout.BeginVertical();
+                if (GUILayout.Button("sample7"))
+                {
+                    Debug.Log("Button7");
+                }
+                GUI.enabled = false;
+                if (GUILayout.Button("sample3"))
+                {
+                    Debug.Log("Button3");
+                }
+                GUILayout.EndVertical();
+
+                if (GUILayout.Button("sample4"))
+                {
+                    Debug.Log("Button4");
+                }
+                GUILayout.EndHorizontal();
+                GUI.enabled = true;
+                if (GUILayout.Button("sample2 Stop Coroutine"))
+                {
+                    Debug.Log("Button2");
+                    isCoroutine = false;
+                }
+                GUILayout.EndScrollView();
+
+                if (GUI.changed)
+                {
+                    Debug.Log("changed");
+                }
+
+            }
+            GUI.enabled = true;
+            GUI.DragWindow(); // 창 드레그 가능하게 해줌. 마지막에만 넣어야함
+        }
+
+        public static bool isCoroutine = false;
+        public static int CoroutineCount = 0;
+
+        private IEnumerator MyCoroutine()
+        {
+            isCoroutine = true;
+            while (isCoroutine)
+            {
+                Sample.log.LogMessage($"MyCoroutine {++CoroutineCount}");
+                //yield return null;
+                yield return new WaitForSeconds(1f);
+            }
+        }
+        /*
+        /// <summary>
+        /// 게임 X 버튼 눌렀을때 반응
+        /// </summary>
+        public void OnApplicationQuit()
+        {
+            Sample.myLog.LogMessage("OnApplicationQuit");
         }
         */
 
-
-        public void OnDisable()
+        private void OnDisable()
         {
-            Sample.myLog.LogMessage("OnDisable");
-
-            //SceneManager.sceneLoaded -= this.OnSceneLoaded;
+            Sample.log.LogMessage("OnDisable");
+            isCoroutine = false;
+            SceneManager.sceneLoaded -= this.OnSceneLoaded;
 
             harmony.UnpatchSelf();// ==harmony.UnpatchAll(harmony.Id);
             //harmony.UnpatchAll(); // 정대 사용 금지. 다름 플러그인이 패치한것까지 다 풀려버림
@@ -150,14 +250,7 @@ namespace BepInPluginSample
             Sample.myLog.LogMessage("Resume");
         }
         */
-        /*
-        /// <summary>
-        /// 게임 X 버튼 눌렀을때 반응
-        /// </summary>
-        public void OnApplicationQuit()
-        {
-            Sample.myLog.LogMessage("OnApplicationQuit");
-        }
-        */
+
+
     }
 }
